2efaac05168b29abe1343b610354feaa70c5f733
eb247 <eb247@hotmail.com>
2018-10-03 14:17:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function add(){},
-function subtract() {},
-function multiply(){},
-function divide(){},
+function add(){a, b}{
+  return a + b
+}
+
+function subtract(a,b) {
+  return a - b
+}
+
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
