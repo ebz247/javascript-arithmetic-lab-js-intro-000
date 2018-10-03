418f2c0fa67329a5aa01594dce29cc3ef9e19501
eb247 <eb247@hotmail.com>
2018-10-03 14:19:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ function multiply(a,b){
 function divide(a,b){
   return a/b
 }
+
+function inc(n){
+  return n +=1
+}
+
+function dec(n){
+  return n -=1
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+
+function preserverDecimal(n) {
+  return parseFloat(n)
+}
